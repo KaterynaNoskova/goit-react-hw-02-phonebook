b71e0594b80273912cfdd47d18626118c5c236ad
KaterynaNoskova <kateryna.noskova@gmail.com>
2023-10-20 12:15:31 +0200

    HW done

--- src/components/ContactToSelect/ContactToSelect.jsx
+++ src/components/ContactToSelect/ContactToSelect.jsx
@@ -2,9 +2,9 @@ import css from './ContactToSelect.module.css';
 
 export const ContactToSelect = ({ name, number, remove, id }) => {
   return (
-    <div className={css.select}>
-      <span className={css.selectName}>
-        {name}:{number}
+    <div>
+      <span>
+        {name} : {number}
       </span>
       <button className={css.buttonDelete} type="button" onClick={() => remove(id)}>
         Delete
